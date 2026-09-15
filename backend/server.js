08aab9dbd8f86963ee7b4a2f472a7ead0590582a
Busari Issa <issabusari66@gmail.com>
2026-09-15 12:46:16 +0100

    Fix authController filename and env config

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 // connect to MongoDB
-mongoose.connect(process.env.MONGO_URL,)
+mongoose.connect(process.env.MONGO_URI,)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
