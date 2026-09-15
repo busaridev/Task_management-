a3c99aaee475db4023a62f8b05a9c3919e4e9bc6
Busari Issa <issabusari66@gmail.com>
2026-09-15 12:56:58 +0100

    Fix authController filename and env configss

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,55 @@
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require('cors')
-const authRoutes = require("./routes/auth");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const taskRoutes = require("./routes/tasks")
+const adminRoutes = require("./routes/admin")
 require('dotenv').config();
 
+
 const app = express();
+// Enable CORS for your Vite frontend
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://task-management-application-teal.vercel.app",
+];
 
-// middleware
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
-app.use(cors());
 
-// connect to MongoDB
-mongoose.connect(process.env.MONGO_URI,)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
-// routes
-app.get("/", (req, res) => {
-  res.send("Task Management API is running ..");                
+// Routes
+app.get('/', (req, res) => {
+    res.send('Task Management API is running...');
 });
 
-// start the server
+
+app.use('/api/auth', authRoutes);
+app.use("/api/tasks", taskRoutes)
+app.use("/api/admin", adminRoutes)
+
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
